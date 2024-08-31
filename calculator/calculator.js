2c5e66c3107634b5e9bb302682af23f683775708
Sai-chidwilas039 <158698168+Sai-chidwilas039@users.noreply.github.com>
2024-08-31 14:26:02 +0530

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,13 +1,10 @@
 const display = document.getElementById("display");
-
 function appendToDisplay(input){
     display.value += input;
 }
-
 function clearDisplay(){
     display.value = "";
 }
-
 function calculate(){
     try{
         display.value = eval(display.value);
